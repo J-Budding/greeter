ce9c3132d25067f7bbffcd74ca57554fb7a01a46
Jochem Budding <budding.jochem@gmail.com>
2018-02-18 22:19:47 +0100

    First commit, Hoera!

---
+++ src/main/java/nl/jochem/greeter/GreetingController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.jochem.greeter;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class GreetingController {
+    
+    private String name = "Jochem";
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot to " + getName();
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
