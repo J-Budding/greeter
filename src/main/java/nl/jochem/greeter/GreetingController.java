f1d467d2f1d3edc88bf2d523937e4dcc439e69a8
Jochem Budding <budding.jochem@gmail.com>
2019-02-08 12:49:35 +0100

    Aanpassing 08022019-1

--- src/main/java/nl/jochem/greeter/GreetingController.java
+++ src/main/java/nl/jochem/greeter/GreetingController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RestController
 public class GreetingController {
 
-    private String name = "JochemZQA";
+    private String name = "JochemZZZ";
 
     @RequestMapping("/")
     public String index() {
